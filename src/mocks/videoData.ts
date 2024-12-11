f45283aed4db733aec22a1943bf9fe6de6fffba4
James Wang <freedom627@163.com>
2024-12-11 18:27:17 +0800

    video upload date

--- src/mocks/videoData.ts
+++ src/mocks/videoData.ts
@@ -1,26 +1,33 @@
 export const mockVideos = [
   {
+    id: 'bgf77YW-Gbk',
+    title: 'Drive mad speedrun 25 level',
+    date: '2024-11-30T14:02:41-08:00'
+  },
+  {
+    id: '2Z-uJLinewg',
+    title: 'Drive Mad Gameplay #2: Avalanche',
+    date: '2024-01-18T07:08:38-08:00'
+  },
+  {
     id: 'Fy3aEKDr_eo',
-    title: 'How to complete sneak past in drive mad'
-  },
-  {
-    id: 'PvURf8oBvjE',
-    title: 'How to beat two high in drive mad'
-  },
-  {
-    id: '9wnzRu-GVsk',
-    title: 'How to beat drive mad level 70 '
+    title: 'How to complete sneak past in drive mad',
+    date: '2023-09-06T12:34:30-07:00'
   },
   {
     id: 'Y3DZdj0VB9k',
     title: 'How to beat runway in drive mad',
+    date: '2023-01-20T11:05:50-08:00'
   },
   {
-    id: 'bgf77YW-Gbk',
-    title: 'Drive mad speedrun 25 level'
+    id: 'PvURf8oBvjE',
+    title: 'How to beat two high in drive mad',
+    date: '2022-11-02T04:00:29-07:00'
   },
   {
-    id: '2Z-uJLinewg',
-    title: 'Drive Mad Gameplay #2: Avalanche'
-  }
+    id: '9wnzRu-GVsk',
+    title: 'How to beat drive mad level 70 ',
+    date: '2022-10-06T14:53:13-07:00'
+  },
 ];
+
